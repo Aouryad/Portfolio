15f7e91198002ebc1755d469fe0617ba9c75f3f8
Aouryad <117912900+Aouryad@users.noreply.github.com>
2025-01-27 11:02:10 +0100

    Update data.js
    linkdin

--- src/section/header/data.js
+++ src/section/header/data.js
@@ -4,7 +4,7 @@ import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
   
-    {id: 3, link: 'https:www.linkedin.com/in/aourtilaneriad', icon: <AiOutlineLinkedin/>},
+    {id: 3, link: 'https://www.linkedin.com/in/aourtilaneriad', icon: <AiOutlineLinkedin/>},
     {id: 4, link: 'https://github.com/Aouryad', icon: <AiFillGithub/>}
 ]
 
